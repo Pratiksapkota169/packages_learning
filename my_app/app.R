4985aceb330c0d450999405cd93f0e38c072dc8b
-ABOER- <493970240@qq.com>
2017-09-05 19:14:43 +0800

    20170905

--- my_app/app.R
+++ my_app/app.R
@@ -1,67 +1,38 @@
-library(shiny)
+library(maps)
+library(mapproj)
+source("helpers.R")
+counties <- readRDS("data/counties.rds")
 
-#Define UI ----
-
-#Shiny uses the function fluidPage to create a display that
-#automatically adjusts to the dimensions of your user's browser
-#window.You lay out the user interface of your app by placing 
-#elements in the fluidPage function.
-
-#For example,the ui function below creates a user interface that
-#has a title panel and a sidebar layout,which includes a sidebar
-#panel and a main panel.Note that these elements are placed within
-#the fluidPage function.
-
-#titlePanel and sidebarLayout are the two most popular elements
-#to add to fluidPage.They create a basic Shiny app with a sidebar.
-#sidebarLayout always takes two arguments:
-#1.sidebarPanel function output
-#2.mainPanel function output
-#These functions place content in either the sidebar or the main panels.
-#The sidebar panel will appear on the left side of your app by default.
-#You can move it to the right by giving sidebarLayout the optional
-#argument position="right".
-
-#titlePanel and sidebarLayout create a basic layout for your Shiny
-#app,but you can also create more advanced layouts.You can use 
-#navbarPage to give you app a multi-page user interface that includes
-#a navigation bar.Or you can use fluidPage and column to build your
-#layout up from a grid system.
-#https://shiny.rstudio.com/articles/layout-guide.html
-
-#Formatted text
-#Shiny offers many tag functions for formatting text.The easier way
-#to describe them is by running through an example.
-
-
-ui<-fluidPage(
-  titlePanel("My Shiny App"),
+# User interface ----
+ui <- fluidPage(
+  titlePanel("censusVis"),
   
   sidebarLayout(
-    sidebarPanel (),
-    mainPanel (
-      h6("Episode IV",align="center"),
-      h6("A NEW HOPE",align="center"),
-      h2("It is a period of civil war.",align="center"),
-      h3("Rebel spaceships, striking",align="center"),
-      h4("from a hidden base, have won",align="center"),
-      h5("their first victory against the",align="center"),
-      h6("evil Galactic Empire.")
-    )
+    sidebarPanel(
+      helpText("Create demographic maps with 
+               information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+      ),
+    
+    mainPanel(plotOutput("map"))
   )
-)
+  )
 
-#Define server logic ----
-server<-function(input,output){
-  
+# Server logic ----
+server <- function(input, output) {
+  output$map <- renderPlot({
+    percent_map( )
+  })
 }
 
-#Run the app ----
-shinyApp(ui = ui,server = server)
-
-
-
-
-
-
-
+# Run app ----
+shinyApp(ui, server)
