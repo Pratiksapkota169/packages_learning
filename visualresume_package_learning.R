4a654a6980299ed9850b085b1b923d28e37a0d22
-ABOER- <493970240@qq.com>
2017-09-09 11:49:24 +0800

    20170909

--- visualresume_package_learning.R
+++ visualresume_package_learning.R
@@ -1,20 +1,21 @@
 #install.packages("devtools")
 devtools::install_github("ndphillips/VisualResume")
 
+
 resume<-VisualResume::VisualResume(
-  titles.left = c("Walter White, PhD", "Chemistry, Cooking, Pizza", "*Built with love in R using the InfoResume package: www.ndphillips.github.io/inforesume"),
-  titles.right = c("www.lospolloshermanos.com", "TheOneWhoKnocks@gmail.com", "Full Resume: www.ndphillips.github.io"),
+  titles.left = c("黄颖 , 硕士", "计算机 , 经济学 ", "*Built with love in R using the InfoResume package: uraboer.github.io"),
+  titles.right = c("个人Blog：https://uraboer.github.io/","工作邮箱：18810600277@163.com", "Github主页：https://github.com/uraboer"),
   titles.right.cex = c(2, 2, 1),
   titles.left.cex = c(4, 2, 1),
-  timeline.labels = c("Education", "Teaching"),
+  timeline.labels = c("教育背景", "工作经历"),
   timeline = data.frame(title = c("Grinnell Col", "Ohio U", "U of Basel", "Max Planck Institute", "Old Van", "Gray Matter", "Sandia Laboratories", "J.P. Wynne High School", "A1A Car Wash"),
                         sub = c("BA. Student", "MS. Student", "PhD. Student", "PhD. Researcher", "Methamphetamine Research", "Co-Founder", "Chemist", "Chemistry Teacher", "Co-Owner"),
                         start = c(1976, 1980.1, 1982.2, 1985, 1996.5, 1987, 1991, 1995, 2001),
                         end = c(1980, 1982, 1985, 1987, 1998, 1992, 1995, 1998, 2003),
                         side = c(1, 1, 1, 1, 1, 0, 0, 0, 0)),
-  milestones = data.frame(title = c("BA", "MS", "PhD"),
-                          sub = c("Mathematics", "Chemistry", "Chemistry"),
-                          year = c(1980, 1982, 1985)),
+  milestones = data.frame(title = c("学士", "硕士", ""),
+                          sub = c("经济学", "软件工程", ""),
+                          year = c(2014, 2017, 1985)),
   events = data.frame(year = c(1985, 1995, 1997, 1999, 2012),
                       title = c("Contributed to Nobel Prize winning experiment.",
                                 "Honorary mention for best Chemistry teacher of the year.",
@@ -26,19 +27,3 @@ resume<-VisualResume::VisualResume(
                    "leadership" = c(rep("Motivation", 10), rep("Decision Making", 5), rep("Manipulation", 30))),
   year.steps = 2
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
