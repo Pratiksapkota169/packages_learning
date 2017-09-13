f30fd4f662a1d0d37332154203791746aaae3bcd
-ABOER- <493970240@qq.com>
2017-09-13 19:07:20 +0800

    20170913

--- re-learn_R in Action.R
+++ re-learn_R in Action.R
@@ -1079,13 +1079,142 @@ head(mydata,n=10)
 #toupper(x)
 #tolower(x)
 
+#length(x)
+#seq(from,to,by)
+#rep(x,n)
+#cut(x,n):divides the continuous variable x into a factor with n levels
+#pretty(x,n)
+#cat(...,file="myfile",append=FALSE)
 
 
+#Applying functions to matrices and data frames
+#Listing 5.4 Applying functions to data objects
+a<-250
+sqrt(a)
+options(digits = 5)#一共显示的位数，重启失效
+
+b<-c(1.243, 5.654, 2.99)
+round(b)
+
+c<-matrix(runif(12),nrow = 3)
+c
+log(c)
+
+mean(c)#takes the average of all 12 elements in the matrix
+
+#Listing 5.5 Applying a function to the rows of a matrix
+mydata<-matrix(rnorm(30),nrow=6)
+mydata
+apply(mydata,1,mean)#calculates the row means
+apply(mydata,2,mean)#calculates the column means
+apply(mydata,2,mean,trim=0.2)
+#means based on the middle 60% of the data,with the bottom 20% and
+#top 20% of the values discarded
 
 
+#Listing 5.6 A solution to the learning example
+options(digits = 2)
+Student <- c("John Davis", "Angela Williams", "Bullwinkle Moose",
+               "David Jones", "Janice Markhammer", "Cheryl Cushing",
+               "Reuven Ytzrhak", "Greg Knox", "Joel England",
+               "Mary Rayburn")
+Math <- c(502, 600, 412, 358, 495, 512, 410, 625, 573, 522)
+Science <- c(95, 99, 80, 82, 75, 85, 80, 95, 89, 86)
+English <- c(25, 22, 18, 15, 20, 28, 15, 30, 27, 18)
+roster <- data.frame(Student, Math, Science, English,
+                       stringsAsFactors=FALSE)
+roster
+z<-scale(roster[,2:4])
+score<-apply(z,1,mean)
+score
+roster<-cbind(roster,score)
+roster
+
+y<-quantile(score,c(.8,.6,.4,.2))
+roster$grade[score>=y[1]]<-"A"
+roster$grade[score<y[1] & score>=y[2]]<-"B"
+roster$grade[score<y[2] & score>=y[3]]<-"C"
+roster$grade[score<y[3] & score>=y[4]]<-"D"
+roster$grade[score<y[4]]<-"F"
+roster
 
 
+name<-strsplit(roster$Student," ")
+name
 
+#"["is a function that extracts part of an object---here the first
+#or second component of the list name
+Lastname<-sapply(name,"[",2)
+Lastname
+
+Firstname<-sapply(name,"[",1)
+Firstname
+
+roster<-cbind(Firstname,Lastname,roster[,-1])
+roster
+
+
+roster<-roster[order(Lastname,Firstname),]
+roster
+
+
+#Listing 5.7 A switch example
+feelings<-c("sad","afraid")
+for(i in feelings){
+  print(
+    switch(i,
+           happy="I am glad you are happy",
+           afraid="There is nothing to fear",
+           sad="Cheer up",
+           angry="Calm down now"
+    )
+  )
+}
+
+
+#Listing 5.8 mystats():a user-written function for summary statistics
+mystats<-function(x,parametric=TRUE,print=FALSE){
+  if(parametric){
+    cneter<-mean(x);spread<-sd(x)
+  }else{
+    center<-median(x);spread<-mad(x)
+  }
+  
+  if(print & parametric){
+    cat("Mean=",center,"\n","SD=",spread,"\n")
+  }else if(print & !paremetric){
+    cat("Median=",center,"\n","MAD=",spread,"\n")
+  }
+  
+  result<-list(center=center,spread=spread)
+  return(result)
+}
+
+
+#Listing 5.9 Transposing a dataset
+cars<-mtcars[1:5,1:4]
+cars
+t(cars)
+
+
+#Listing 5.10 Aggregating data
+options(digits = 3)
+attach(mtcars)
+aggdata<-aggregate(mtcars,by=list(cyl,gear),FUN=mean,na.rm=TRUE)
+aggdata
+
+
+#install.packages("reshape2")
+library(reshape2)
+
+
+#Bar plots
+install.packages("vcd")
+library(vcd)
+
+counts<-table()
+
+#page 118
 
 
 
