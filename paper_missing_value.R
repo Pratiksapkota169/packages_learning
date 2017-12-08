39586703c0926bb5aec50a5a1cf97b3a117ada13
uraboer <493970240@qq.com>
2017-12-08 18:31:11 +0800

    20171208

---
+++ paper_missing_value.R
@@ -0,0 +1,31 @@
+#learn from:http://mp.weixin.qq.com/s?__biz=MzA3MTM3NTA5Ng==&mid=2651057172&idx=1&sn=0dd2a3b95acf91a5d17fe37f47b4cf4d&chksm=84d9cd83b3ae4495926a40b020c12de4a89d0bd863aa4e45b29128f3a65264af3ee86378af40&mpshare=1&scene=1&srcid=1205c43vLeUWQ7hG0pNCNjP2#rd
+
+#1.1缺失值的分类
+#缺失值从数据分布上可被分为三类：
+#完全随机缺失(missing completely at random,MCAR):某一变量缺失值不依赖于其他任何原因的完全随机缺失
+#随机缺失(missing at random,MAR):某一变量的缺失与其他变量相关但与该变量本身的数值不相关
+#非随机缺失(missing not at random,NMAR):某一变量的缺失和该变量本身的数值相关
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ paper_sql.R
@@ -0,0 +1,12 @@
+#learn from:http://mp.weixin.qq.com/s?__biz=MzA3Njc0NzA0MA==&mid=2653192156&idx=1&sn=765d96574f98781cb0c578a95c6191a3&chksm=848c4793b3fbce85a18b84a5982274368ceb80f2fcb51592a270637cda6d022dcf674a7ff109&mpshare=1&scene=1&srcid=1208D89tRTPByvSPtgNjqjaS#rd
+
+
+
+
+
+
+
+
+
+
+
